4926513ea025cf719d790598a89cf0b76397a2d7
Sergey <kobetssergeyapp@gmail.com>
2018-03-12 21:30:15 +0200

    Lessons

--- src/lessons/lesson1/task2/Sort.java
+++ src/lessons/lesson1/task2/Sort.java
@@ -4,14 +4,20 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array = new int[]{3,5,-3,0,6,-1,5,6,2,4,-9,4,7,0,1,8,7,3,1,-2,5,9,7,4,0,2,-6};
-        System.out.println(Arrays.toString(quickSort(array, 0, array.length - 1)));
+        int[] array = new int[]{3, 5, -3, 0, 6, -1, 5, 6, 2, 4, -9, 4, 7, 0, 7, 4, 0, 2, -6, 9};
+        int[] array1 = new int[]{3, 5, -3, 0, 6, -1, 5, 6, 2, 4};
+
+        System.out.println("Before sort: " + Arrays.toString(array));
+
+        quickSort(array, 0, array.length - 1);
+
+        System.out.println("After sort: " +  Arrays.toString(array));
     }
 
 
-    private static int[] quickSort(int[] arr, int low, int high) {
-        if (arr.length <= 0) return arr;
-        if (low >= high) return arr;
+    private static void quickSort(int[] arr, int low, int high) {
+        if (arr.length <= 0) return;
+        if (low >= high) return;
         int left = low;
         int right = high;
 
@@ -33,7 +39,6 @@ public class Sort {
         quickSort(arr, low, left - 1);
         quickSort(arr, left + 1, high);
 
-        return arr;
     }
 
 
