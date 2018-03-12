6ae6fd267c7484942c413b224d5461fc8ec9bd15
Sergey <kobetssergeyapp@gmail.com>
2018-03-13 01:48:34 +0200

    Lessons

--- src/lessons/lesson1/task2/Sort.java
+++ src/lessons/lesson1/task2/Sort.java
@@ -4,14 +4,20 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array = new int[]{3, 5, -3, 0, 6, -1, 5, 6, 2, 4, -9, 4, 7, 0, 7, 4, 2, -6, 9};
-        int[] array1 = new int[]{3, 5, -3, 0, 6, -1, 5, 6, 2, 4};
+        int[] array1 = {3, 5, -3, 0, 6, -1, 5, 6, 2, 4, -9, 4, 7, 0, 7, 4, 2, -6, 9};
+        int[] array2 = {5, 4, -3, 2, 1, -1, 0, -2, 3, -4, 5, 100, 22, -4};
 
-        System.out.println("Before sort: " + Arrays.toString(array));
+        System.out.println("Before sort: " + Arrays.toString(array1));
 
-        quickSort(array, 0, array.length - 1);
+        quickSort(array1, 0, array1.length - 1);
 
-        System.out.println("After sort: " +  Arrays.toString(array));
+        System.out.println("After sort: " +  Arrays.toString(array1));
+
+        System.out.println("Before SortSequence: " + Arrays.toString(array2));
+
+        sortSequence(array2);
+
+        System.out.println("Fgter SortSequence: " + Arrays.toString(array2));
     }
 
 
@@ -39,6 +45,37 @@ public class Sort {
         quickSort(arr, low, left - 1);
         quickSort(arr, left + 1, high);
 
+    }
+
+
+    public static void sortSequence(int[] arr) {
+        int len = arr.length - 1;
+        int first = 0;
+
+        for (int last = len; last > 0 && first != last; --last) {
+            if (arr[last] > 0) {
+                while (first < len) {
+                    if (arr[first] < 0) {
+                        swap(arr, last, first);
+                        ++first;
+                        break;
+                    } else {
+                        ++first;
+                    }
+
+                }
+            }
+        }
+
+
+
+    }
+
+
+    public static void swap(int[] items, int left, int right) {
+        int temp = items[left];
+        items[left] = items[right];
+        items[right] = temp;
     }
 
 
