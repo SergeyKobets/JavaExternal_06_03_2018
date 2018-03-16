15d5cf7ceabd047fa1426ea6a079b03e4ba91c58
Sergey <kobetssergeyapp@gmail.com>
2018-03-16 20:16:56 +0200

    Homework

---
+++ src/lessons/lesson3/Main.java
@@ -0,0 +1,25 @@
+package lessons.lesson3;
+
+import lessons.lesson3.model.Car;
+import lessons.lesson3.model.Plane;
+import lessons.lesson3.model.Ship;
+import lessons.lesson3.model.Vehicle;
+
+public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+
+        Vehicle[] listVehicle = new Vehicle[8];
+        listVehicle[0] = new Plane(5000, 2006, 800, 10, 29, 8000, 350);
+        listVehicle[1] = new Car(100, 2017, 300, 56, 90);
+        listVehicle[2] = new Car(303, 2006, 300, 56, 90);
+        listVehicle[3] = new Car(530, 2014, 300, 56, 90);
+        listVehicle[4] = new Ship(100, 2004, 160, 14, 76, "Barсelona port", 100);
+        listVehicle[5] = new Ship(130, 2014, 160, 14, 76, "Barсelona port", 160);
+        listVehicle[6] = new Ship(140, 2015, 160, 14, 76, "Barсelona port", 170);
+        listVehicle[7] = new Ship(150, 2016, 160, 14, 76, "Barсelona port", 200);
+
+        controller.execute(listVehicle);
+
+    }
+}
---
+++ src/lessons/lesson3/model/Plane.java
@@ -0,0 +1,27 @@
+package lessons.lesson3.model;
+
+public class Plane extends Vehicle {
+    private int height;
+    private int numberOfPassengers;
+
+    public Plane(int price, int year, int speed, int x, int y, int height, int numberOfPassengers) {
+        super("Plane", price, year, speed, x, y);
+        this.height = height;
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", height=" + height +
+                ", numberOfPassengers=" + numberOfPassengers;
+    }
+}
