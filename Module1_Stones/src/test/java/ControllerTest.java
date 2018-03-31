0c30893072bbde47fd21d5855191c32386e1fb7b
admin <kobetssergeyapp@gmail.com>
2018-04-01 00:16:28 +0300

    Homework

---
+++ Module1_Stones/src/test/java/ControllerTest.java
@@ -0,0 +1,43 @@
+import exception.WrongOperationException;
+import model.Gemstone;
+import model.Stone;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+    private Controller controller;
+
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void initController() {
+        controller = new Controller();
+    }
+
+    @After
+    public void clearController() {
+        controller = null;
+    }
+
+    @Test
+    public void executeForOtherTest() throws WrongOperationException, IOException {
+        thrown.expect(WrongOperationException.class);
+        controller.execute(Operation.OTHER);
+    }
+
+    @Test
+    public void selectNotNullStonesTest() {
+        Stone[] stones = {new Gemstone("", 3, 4, 5), null, null, null};
+        int expect = 1;
+        Stone[] actual = controller.selectNotNullStones(stones);
+        assertEquals(expect, actual.length);
+    }
+}
