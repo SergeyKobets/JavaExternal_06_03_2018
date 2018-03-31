6523eb2214c51c99eeff3c1c49ff25d86dd42ff9
admin <kobetssergeyapp@gmail.com>
2018-03-31 23:43:18 +0300

    Homework

--- src/lessons/lesson2/task2/Polygon.java
+++ src/lessons/lesson2/task2/Polygon.java
@@ -1,24 +1,47 @@
 package lessons.lesson2.task2;
 
-import lessons.lesson2.task1.RandomShapeGenerator;
-import lessons.lesson2.task1.Shape;
+import lessons.lesson2.task1.*;
 
-public class Polygon {
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class Polygon implements Serializable {
+    private static int ID = 0;
     private static RandomShapeGenerator gen = new RandomShapeGenerator();
-    Shape[] figures;
+    private Shape[] figures;
 
     public Polygon(int len) {
+        ++ID;
         this.figures = new Shape[len];
+        initShapes();
     }
 
 
-    public void drawShapes() {
+    public Shape[] getFigures() {
+        return figures;
+    }
+
+    public void setFigures(Shape[] figures) {
+        this.figures = figures;
+    }
+
+    private void initShapes() {
         for (int i = 0; i < figures.length; i++) {
             figures[i] = gen.next();
         }
+    }
 
+    public void drawShapes() {
         for (Shape shp : figures) {
             shp.draw();
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Polygon " + ID +
+                " {" +
+                "figures=" + Arrays.toString(figures) +
+                '}';
     }
 }
